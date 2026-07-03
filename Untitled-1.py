b3d1ac9c914f9497c5bdb82efdec9da6212b8da1
nishatariq567-cpu <nishatariq567@gmail.com>
2026-07-03 23:43:17 +0500

    practices code

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+if number > 10:
+    print("the number is greater than 10")
+    else:
+        print("the num er")
---
+++ practices python.py
@@ -0,0 +1,6 @@
+name="nisha"
+age=20
+height=5.6
+is_student=true
+print(name)
+print(age)
